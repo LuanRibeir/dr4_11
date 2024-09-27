3ca841d4b4ca2c643286899b777ba074fa6b561d
LuanRibeir <luaneribeiroalmeida@gmail.com>
2024-09-27 20:40:38 -0300

    Fix dependencies

--- dr4_11/src/main/java/com/luanribeiro/dr4_11/Dr411Application.java
+++ dr4_11/src/main/java/com/luanribeiro/dr4_11/Dr411Application.java
@@ -2,9 +2,7 @@ package com.luanribeiro.dr4_11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class Dr411Application {
 
